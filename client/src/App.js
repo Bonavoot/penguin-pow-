35f850281f5d9dc2fbc7a7a5cb6fc94453cfef93
Joseph <Jbonventre95@gmail.com>
2023-03-01 00:46:47 -0500

    Movement ehancements and dive animation

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import standing from './assets/Standing.GIF';
 import jumping from './assets/jump.gif'
 import map from './assets/map.png';
 import attack from './assets/attack.gif'
+import diving from './assets/diving.gif'
 
 const socket = io('http://localhost:3001');
 
@@ -17,6 +18,7 @@ function App() {
     Space: false,
   });
   const [players, setPlayers] = useState([]);
+  const [timer, setTimer] = useState(99)
   const keysPressed = useRef({});
 
   useEffect(() => {
@@ -29,7 +31,7 @@ function App() {
     const handleKeyDown = (e) => {
       keysPressed.current[e.code] = true;
       console.log(e.code)
-      const { ArrowLeft, ArrowRight, ArrowUp, Space } = keysPressed.current;
+      const { ArrowLeft, ArrowDown, ArrowRight, ArrowUp, Space } = keysPressed.current;
 
       if (ArrowUp && !inputs.ArrowUp) {
         setInputs({ ArrowUp: true, ArrowDown: false, ArrowLeft: ArrowLeft, ArrowRight: ArrowRight, Space: Space,});
@@ -44,6 +46,10 @@ function App() {
         setInputs({ ArrowUp: ArrowUp, ArrowDown: false, ArrowLeft: ArrowLeft, ArrowRight: ArrowRight, Space: true,  })
       } else if (Space && inputs.Space) {
         setInputs(({ ArrowUp: ArrowUp, ArrowDown: false, ArrowLeft: ArrowLeft, ArrowRight: ArrowRight, Space: inputs.Space, }))
+      } else if (ArrowDown && !ArrowDown.ArrowDown) {
+        setInputs({ ArrowUp: false, ArrowDown: true, ArrowLeft: ArrowLeft, ArrowRight: ArrowRight, Space: Space,  })
+      } else if (ArrowDown && inputs.ArrowDown) {
+        setInputs(({ ArrowUp: false, ArrowDown: inputs.ArrowDown, ArrowLeft: ArrowLeft, ArrowRight: ArrowRight, Space: Space, }))
       }
     };
 
@@ -51,11 +57,11 @@ function App() {
     const handleKeyUp = (e) => {
       keysPressed.current[e.code] = false;
 
-      const { ArrowLeft, ArrowRight, ArrowUp, Space } = keysPressed.current;
+      const { ArrowLeft, ArrowRight, ArrowDown, ArrowUp, Space } = keysPressed.current;
 
       setInputs({
         ArrowUp: ArrowUp,
-        ArrowDown: false,
+        ArrowDown: ArrowDown,
         ArrowLeft: ArrowLeft,
         ArrowRight: ArrowRight,
         Space: Space,
@@ -83,30 +89,30 @@ function App() {
 
   }, []);
 
-  
+  useEffect(() => {
+    socket.on('timer', (time) => {
+      if(time <= 0){
+        setTimer(0)
+      } else {
+        setTimer(time)
+      }
+    });
+  }, []);
+
 
   return (
     <div className="App">
       <img className='map' src={map} alt="map" />
+      <div className='timer'>{timer}</div>
       {players.map((player, i) => {
         return (
           <>
-          {player.attacking ? <img
-          className='players'
-            key={player.id}
-            style={{ position: 'absolute', left: player.x, bottom: player.y, width: "285px", transform: `scaleX(${player.facingRight})`, transition: `left .45s ease-out`}}
-            src={attack}
-            alt="standing"
-          />  :
-          <img
-          className='players'
-            key={player.id}
-            style={{ position: 'absolute', left: player.x, bottom: player.y, width: "175px", transform: `scaleX(${player.facingRight})`, transition: `left .45s ease-out`}}
-            src={player.jumping ? jumping : standing }
-            alt="standing"
-          /> }
-          
-          <div className={"hp" + i}>{player.hp}</div> 
+          {renderAnimationImg(player)}
+          <div className={'hp-container' + i}>
+          <h2 id={"player-name-hp" + i} style={{position: "absolute", top: "-50px", left: i === 0 ? "10px" : "170px"}}>{"Player " + (i + 1)}</h2>
+          <div className={"hp" + i} style={{width: player.hp + '%'}}>
+          </div> 
+          </div>
           </>
         );
       })}
@@ -117,3 +123,58 @@ function App() {
 export default App;
 
 
+
+
+const renderAnimationImg = (player) => {
+const attackImgWidth = 290; // width of attacking image
+const standingImgWidth = 175; // width of standing image
+const attackImgHeight = 252;
+const standingImgHeight = 188;
+
+const widthDiff = (attackImgWidth - standingImgWidth -100) / 2;
+const heightDiff = (attackImgHeight - standingImgHeight - 30) / 2;
+  if (player.attacking) {
+     return <img className='players'
+      key={player.id}
+      style={{ position: 'absolute',
+      left: player.x - widthDiff, // offset position to center attacking image
+      bottom: player.y - heightDiff,
+      width: `${attackImgWidth}px`,
+      height: `${attackImgHeight}px`, 
+      transform: `scaleX(${player.facingRight})`, 
+      transition: `left .45s ease-out`}}
+      src={attack}
+      alt="attack"
+    />
+  } else if (player.jumping) {
+    return  <img
+    className='players'
+      key={player.id}
+      style={{ position: 'absolute', left: player.x, bottom: player.y, width: "175px", transform: `scaleX(${player.facingRight})`, transition: `left .45s ease-out`}}
+      src={jumping}
+      alt="jumping"
+    />
+  } else if (player.diving) {
+   return <img
+    className='players'
+    key={player.id}
+    style={{ position: 'absolute', left: player.x, bottom: player.y, width: "175px", transform: `scaleX(${player.facingRight})`, transition: `left .45s ease-out`}}
+    src={diving}
+    alt="diving"
+  />
+  } else {
+   return <img
+    className='players'
+    key={player.id}
+    style={{ position: 'absolute', 
+    left: player.x, 
+    bottom: player.y,
+    width: `${standingImgWidth}px`,
+    height: `${standingImgHeight}px`, 
+    transform: `scaleX(${player.facingRight})`, 
+    transition: `left .45s ease-out`}}
+    src={standing}
+    alt="standing"
+  />
+  }
+}
--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-
-const TICK_RATE = 45;
+const TICK_RATE = 40;
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -22,19 +21,20 @@ for (let i = 0; i < 1000; i++) {
   pool.push({
     id: null,
     x: 0,
-    y: 0,
+    y: 65,
     attackCooldown: 0,
     facingRight: 1,
     jumping: false,
+    diving: false,
     attacking: false,
     attackStartTime: null,
-    hp: 3,
+    hp: 100,
   });
 }
 
+
 let players = [];
 let isSecondPlayerJoined = false;
-
 
 // this tick function is being run in a setInterval function to allow smoother framerate transitions on different hardware capabilities
 function tick(delta) {
@@ -54,54 +54,90 @@ function tick(delta) {
     // this handles the jump input y axis, and the left/right movement input on the x axis 
     // if statements allow players to move left/right while jumping
     // velocity determines how far penguin will jump depending on current speed
+
     if (input.ArrowUp && !player.jumping) {
+      if(player.y > 65){
+        player.jumping = false
+      }
       player.jumping = true;
       player.yVelocity = 25;
     }
-
-    if (player.jumping) {
+    
+    if (player.jumping && !player.diving) {
+        if(player.y > 65){
+            player.diving = false
+          }
       player.yVelocity -= 1;
       player.y += player.yVelocity;
-      if (player.y < 0) {
-        player.y = 0;
+      if (player.y < 65) {
+        player.y = 65;
         player.jumping = false;
       }
     }
 
-    const ATTACK_DURATION = 1000;
+    if (input.ArrowDown && !player.diving ) {
+        player.diving = true;
+        player.jumping = false;
+      }
+      
+      if (player.diving) {
+        if (player.y <= 65) {
+          player.y = 65;
+        } else {
+          player.y -= 25;
+        }
+        player.jumping = false;
+      }
+    //   if (player.diving) {
+    //     player.y -= 25;
+    //     player.jumping = false;
+    //     if(player.y < 65) {
+    //       player.y = 65;
+    //       player.diving = false;
+    //      } 
 
     if(input.Space && !player.attacking){
-      player.attacking = true
-      player.attackCooldown = 20;
+      player.attacking = true;
+      player.attackCooldown = 10;
       player.attackStartTime = Date.now();
         
          // handle attack hit detection
          for (let j = 0; j < players.length; j++) {
             const otherPlayer = players[j];
           
-            // calculate distance between players in both x and y axes
-            const distanceX = Math.abs(player.x - otherPlayer.x);
-            const distanceY = Math.abs(player.y - otherPlayer.y);
-            
+        // calculate factor based on movement direction
+        const factor = input.ArrowLeft || input.ArrowRight ? 1.5 : 1.5;
+
+// calculate distance with factor
+        const distanceX = Math.abs(player.x - otherPlayer.x) * factor;
+        const distanceY = Math.abs(player.y - otherPlayer.y);
             // check if players are close enough to hit each other
-            if (otherPlayer.id !== player.id && distanceX < 180 && distanceY < 180) {
+            if (otherPlayer.id !== player.id && distanceX < 195 && distanceY < 225) {
               if (player.facingRight === 1 && otherPlayer.x > player.x || player.facingRight === -1 && otherPlayer.x < player.x) {
                 // player is facing the other player
                 if (player.attackStartTime + player.attackCooldown > Date.now()) {
                     console.log("hit");
-                    otherPlayer.hp -= 1; // subtract 1 hit point
+                    otherPlayer.hp -= 20; // subtract 20% of health
+                    if(player.facingRight === 1){
+                        otherPlayer.x += 350; // knock back
+                    } else {
+                        otherPlayer.x -= 300
+                    }
+                    
                     if (otherPlayer.hp <= 0) {
-                      console.log("dead");
+                        otherPlayer.hp = 0
+                        console.log("dead");
                     }
                   }
               }
             }
-          }
-          
+          }  
     }
 
     if(player.attacking){
-      //handle collisions here ???
+        if(player.y <= 65){
+            player.diving = false;
+          }
       player.attackCooldown -= 1
       if(player.attackCooldown < 0) {
         player.attackCooldown = 0;
@@ -110,9 +146,16 @@ function tick(delta) {
     }
 
     if (input.ArrowLeft) {
+      if(player.y <= 65){
+        player.diving = false;
+      }
+      
       player.x -= delta;
       player.facingRight = -1;
     } else if (input.ArrowRight) {
+        if(player.y <= 65){
+            player.diving = false;
+          }
       player.x += delta;
       player.facingRight = 1;
     }
@@ -131,7 +174,6 @@ function tick(delta) {
       io.emit("players", players);
   }
 
-
 let numPlayers = 0;
 
 io.on("connection", (socket) => {
@@ -145,28 +187,43 @@ io.on("connection", (socket) => {
       Space: false
     };
 
+
+    let time = 99
+    let startCount = 3
+    const gameClock = setInterval(() => {
+    time -= 1
+    socket.emit("timer", time)
+    }, 1000)
+   
+    if(time <= 0){
+    time = 0;
+    clearInterval(gameClock)
+}
+    
     let player = pool.pop();
     if (!player) {
       player = {
         id: socket.id,
         facingRight: 1,
         x: 0,
-        y: 0,
+        y: 65,
         attackCooldown: 0,
         jumping: false,
+        diving: false,
         attacking: false,
-        hp: 3,
+        hp: 100,
         attackStartTime: null,
       };
     } else {
       player.id = socket.id;
       player.x = 25;
-      player.y = 0;
+      player.y = 65;
       player.attackCooldown = 0,
       player.facingRight = 1;
       player.jumping = false;
+      player.diving = false;
       player.attacking = false;
-      player.hp = 3;
+      player.hp = 100;
       player.attackStartTime = null;
     }
     players.push(player);
