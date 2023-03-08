e52a4829fe3c1a6b02f9a7624feb203210edc77a
Joseph <Jbonventre95@gmail.com>
2023-03-08 11:28:28 -0500

    formatted server file

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-const TICK_RATE = 50;
+const TICK_RATE = 45;
 
 const server = http.createServer(app);
 const io = new Server(server, {
